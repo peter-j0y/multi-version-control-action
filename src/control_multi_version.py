11a4c471dc681470160cdf85a44cb8f63fef7f09
peter-j0y <tmdals8798@gmail.com>
2024-01-26 13:41:03 +0900

    revision update 추가

--- src/control_multi_version.py
+++ src/control_multi_version.py
@@ -1,5 +1,12 @@
 import re
 import os
+from enum import Enum, auto
+
+class VersionCategory(Enum):
+    REVISION = auto()
+    PATCH = auto()
+    MINOR = auto()
+    MAJOR = auto()
 
 # version_name 읽는 함수
 def read_gradle_version_name(gradle_file_path, variable_name):
@@ -56,105 +63,59 @@ def write_gradle_version(vc_variable_name, vn_variable_name, new_version_code, n
 def decode_labels(labels, version_name_dev, version_name_stg, version_name_prod, version_code_dev, version_code_stg, version_code_prod):
     label_list = labels.split(' ')
     print(label_list)
+
+    args = {
+        'dev': {'version_code': version_code_dev, 'version_name': version_name_dev, 'version_code_variable_name': version_code_dev_variable_name, 'version_name_variable_name': version_name_dev_variable_name},
+        'stg': {'version_code': version_code_stg, 'version_name': version_name_stg, 'version_code_variable_name': version_code_stg_variable_name, 'version_name_variable_name': version_name_stg_variable_name},
+        'prod': {'version_code': version_code_prod, 'version_name': version_name_prod, 'version_code_variable_name': version_code_prod_variable_name, 'version_name_variable_name': version_name_prod_variable_name}
+    }
+    
+    # dev, stg, prod 일괄적으로 bump
     if 'all' in label_list:
-        version_code_dev = int(version_code_dev); version_code_stg = int(version_code_stg); version_code_prod = int(version_code_prod)
-        major_version_dev = int(version_name_dev.split('.')[0]); minor_version_dev = int(version_name_dev.split('.')[1]); patch_version_dev = int(version_name_dev.split('.')[2])
-        major_version_stg = int(version_name_stg.split('.')[0]); minor_version_stg = int(version_name_stg.split('.')[1]); patch_version_stg = int(version_name_stg.split('.')[2])
-        major_version_prod = int(version_name_prod.split('.')[0]); minor_version_prod = int(version_name_prod.split('.')[1]); patch_version_prod = int(version_name_prod.split('.')[2])
-
-        if 'bump-patch' in label_list:
-            bump_patch(version_code_dev_variable_name, version_name_dev_variable_name, version_code_dev, major_version_dev, minor_version_dev, patch_version_dev)
-            bump_patch(version_code_stg_variable_name, version_name_stg_variable_name, version_code_stg, major_version_stg, minor_version_stg, patch_version_stg)
-            bump_patch(version_code_prod_variable_name, version_name_prod_variable_name, version_code_prod, major_version_prod, minor_version_prod, patch_version_prod)
-
-        if 'bump-minor' in label_list:
-            bump_minor(version_code_dev_variable_name, version_name_dev_variable_name, version_code_dev, major_version_dev, minor_version_dev, patch_version_dev)
-            bump_minor(version_code_stg_variable_name, version_name_stg_variable_name, version_code_stg, major_version_stg, minor_version_stg, patch_version_stg)
-            bump_minor(version_code_prod_variable_name, version_name_prod_variable_name, version_code_prod, major_version_prod, minor_version_prod, patch_version_prod)
-
-        if 'bump-major' in label_list:
-            bump_major(version_code_dev_variable_name, version_name_dev_variable_name, version_code_dev, major_version_dev, minor_version_dev, patch_version_dev)
-            bump_major(version_code_stg_variable_name, version_name_stg_variable_name, version_code_stg, major_version_stg, minor_version_stg, patch_version_stg)
-            bump_major(version_code_prod_variable_name, version_name_prod_variable_name, version_code_prod, major_version_prod, minor_version_prod, patch_version_prod)
-
+        for arg in args.values():
+            update_version(arg, label_list)
+    # 선택한 빌드 환경만 bump
     else:
-        if 'dev' in label_list and version_code_dev is not None and version_name_dev is not None:
-            version_code = int(version_code_dev)
-            major_version = int(version_name_dev.split('.')[0])
-            minor_version = int(version_name_dev.split('.')[1])
-            patch_version = int(version_name_dev.split('.')[2])
+        for arg_key, arg in args.items():
+            if arg_key in label_list and arg['version_code'] is not None and arg['version_name'] is not None:
+                update_version(arg, label_list)
 
-            if 'bump-patch' in label_list:
-                bump_patch(version_code_dev_variable_name, version_name_dev_variable_name, version_code, major_version, minor_version, patch_version)
-            if 'bump-minor' in label_list:
-                bump_minor(version_code_dev_variable_name, version_name_dev_variable_name, version_code, major_version, minor_version, patch_version)
-            if 'bump-major' in label_list:
-                bump_major(version_code_dev_variable_name, version_name_dev_variable_name, version_code, major_version, minor_version, patch_version)
-                
+def update_version(arg, label_list):
+    version_code = int(arg['version_code'])
+    major_version, minor_version, patch_version, revision_version = map(int, arg['version_name'].split('.'))
 
-        if 'stg' in label_list and version_code_stg is not None and version_name_stg is not None:
-            version_code = int(version_code_stg)
-            major_version = int(version_name_stg.split('.')[0])
-            minor_version = int(version_name_stg.split('.')[1])
-            patch_version = int(version_name_stg.split('.')[2])
-
-            if 'bump-patch' in label_list:
-                bump_patch(version_code_stg_variable_name, version_name_stg_variable_name, version_code, major_version, minor_version, patch_version)
-            if 'bump-minor' in label_list:
-                bump_minor(version_code_stg_variable_name, version_name_stg_variable_name, version_code, major_version, minor_version, patch_version)
-            if 'bump-major' in label_list:
-                bump_major(version_code_stg_variable_name, version_name_stg_variable_name, version_code, major_version, minor_version, patch_version)
-
-
-        if 'prod' in label_list and version_code_prod is not None and version_name_prod is not None:
-            version_code = int(version_code_prod)
-            major_version = int(version_name_prod.split('.')[0])
-            minor_version = int(version_name_prod.split('.')[1])
-            patch_version = int(version_name_prod.split('.')[2])
-
-            if 'bump-patch' in label_list:
-                bump_patch(version_code_prod_variable_name, version_name_prod_variable_name, version_code, major_version, minor_version, patch_version)
-            if 'bump-minor' in label_list:
-                bump_minor(version_code_prod_variable_name, version_name_prod_variable_name, version_code, major_version, minor_version, patch_version)
-            if 'bump-major' in label_list:
-                bump_major(version_code_prod_variable_name, version_name_prod_variable_name, version_code, major_version, minor_version, patch_version)
+    for version_type in list(VersionCategory):
+        if f'bump-{version_type.name.lower()}' in label_list:
+            bump_version(version_type, arg['version_code_variable_name'], arg['version_name_variable_name'], version_code, major_version, minor_version, patch_version, revision_version)
 
 # 버전 올리는 함수
-def bump_patch(vc_variable_name, vn_variable_name, version_code, major_version, minor_version, patch_version):
-    print(f"Current version : {major_version}.{minor_version}.{patch_version}")
+def bump_version(version_category, vc_variable_name, vn_variable_name, version_code, major_version, minor_version, patch_version, revision_version) :
+    print(f"Current version : {major_version}.{minor_version}.{patch_version}.{revision_version}")
     env_file = os.getenv('GITHUB_ENV')
+
     with open(env_file, "a") as my_file:
-        my_file.write(f"CURRENT_VERSION_NAME={major_version}.{minor_version}.{patch_version}\n")
+        my_file.write(f"CURRENT_VERSION_NAME={major_version}.{minor_version}.{patch_version}.{revision_version}\n")
         my_file.write(f"CURRENT_VERSION_CODE={version_code}\n")
 
-    patch_version += 1
+    # version_code는 항상 증가
     version_code += 1
-    write_gradle_version(vc_variable_name, vn_variable_name, version_code, f"{major_version}.{minor_version}.{patch_version}")
 
-def bump_minor(vc_variable_name, vn_variable_name, version_code, major_version, minor_version, patch_version):
-    print(f"Current version : {major_version}.{minor_version}.{patch_version}")
-    env_file = os.getenv('GITHUB_ENV')
-    with open(env_file, "a") as my_file:
-        my_file.write(f"CURRENT_VERSION_NAME={major_version}.{minor_version}.{patch_version}\n")
-        my_file.write(f"CURRENT_VERSION_CODE={version_code}\n")
-
-    patch_version = 0
-    minor_version += 1
-    version_code += 1
-    write_gradle_version(vc_variable_name, vn_variable_name, version_code, f"{major_version}.{minor_version}.{patch_version}")
-
-def bump_major(vc_variable_name, vn_variable_name, version_code, major_version, minor_version, patch_version):
-    print(f"Current version : {major_version}.{minor_version}.{patch_version}")
-    env_file = os.getenv('GITHUB_ENV')
-    with open(env_file, "a") as my_file:
-        my_file.write(f"CURRENT_VERSION_NAME={major_version}.{minor_version}.{patch_version}\n")
-        my_file.write(f"CURRENT_VERSION_CODE={version_code}\n")
-
-    major_version += 1
-    minor_version = 0
-    patch_version = 0
-    version_code += 1
-    write_gradle_version(vc_variable_name, vn_variable_name, version_code, f"{major_version}.{minor_version}.{patch_version}")
+    if version_category is VersionCategory.REVISION:
+        revision_version += 1
+    elif version_category is VersionCategory.PATCH:
+        revision_version = 0
+        patch_version += 1
+    elif version_category is VersionCategory.MINOR:
+        revision_version = 0
+        patch_version = 0
+        minor_version += 1
+    elif version_category is VersionCategory.MAJOR:
+        revision_version = 0
+        patch_version = 0
+        minor_version = 0
+        major_version += 1
+        
+    write_gradle_version(vc_variable_name, vn_variable_name, version_code, f"{major_version}.{minor_version}.{patch_version}.{revision_version}")
 
 if __name__ == '__main__':
     gradle_file_path = os.environ.get('FILE-PATH')
