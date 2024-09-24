4825a64cf69a3e7ea34d539e7f5a95347e8692f8
peter-j0y <tmdals8798@gmail.com>
2024-09-24 10:05:22 +0900

    pull origin main

--- src/control_multi_version.py
+++ src/control_multi_version.py
@@ -89,7 +89,7 @@ def update_version(arg, label_list):
     isBumpLabelNotExist = True
     for version_type in list(VersionCategory):
         if f'bump-{version_type.name.lower()}' in label_list:
-            isBumpLabelExist = false
+            isBumpLabelExist = False
             bump_version(version_type, arg['version_code_variable_name'], arg['version_name_variable_name'], version_code, major_version, minor_version, patch_version, revision_version)
 
     if isBumpLabelNotExist:
